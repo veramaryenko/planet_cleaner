f154ee20ff50f759f527dcbbab2024a8294b88ee
veramaryenko <verunga15@mail.ru>
2019-12-05 08:25:31 +0100

    add image

--- lib/ui/camera/gallery_and_camera.dart
+++ lib/ui/camera/gallery_and_camera.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nice_button/nice_button.dart';
 import 'package:planet_cleaner/utils/app_color.dart';
 import 'package:image_picker/image_picker.dart';
@@ -11,90 +14,155 @@ class CameraTestWidget extends StatefulWidget {
 }
 
 class _CameraTestWidgetState extends State<CameraTestWidget> {
-
   File imageFile;
 
-  _openGallery(BuildContext context) async{
+  String _currentItemSelected = 'soil pollution';
+  var _pollutians = ['air pollution','water pollution','soil pollution','light pollution'];
+
+
+  _openGallery(BuildContext context) async {
     var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
-    this.setState((){
+    this.setState(() {
       imageFile = picture;
     });
     Navigator.of(context).pop();
   }
 
-  _openCamera(BuildContext context)async{
+  _openCamera(BuildContext context) async {
     var picture = await ImagePicker.pickImage(source: ImageSource.camera);
-    setState((){
+    setState(() {
       imageFile = picture;
     });
     Navigator.of(context).pop();
   }
 
-  Future<void> _showChoiceDialog(BuildContext context){
-    return showDialog(context: context, builder: (BuildContext context){
-      return AlertDialog(
-        title: Text("Make a choice"),
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              GestureDetector(
-                child: Text("Gallery"),
-                onTap: (){
-                  _openGallery(context);
-                } ,
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Make a choice"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text("Gallery"),
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8.0)),
+                  GestureDetector(
+                    child: Text("Camera"),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  )
+                ],
               ),
-              Padding(padding: EdgeInsets.all(8.0)),
-              GestureDetector(
-                child: Text("Camera"),
-                onTap: (){
-                  _openCamera(context);
-                } ,
-              )
+            ),
+          );
+        });
+  }
+
+
+  final _formKey = GlobalKey<FormState>();
+  Widget _decideImageView() {
+
+    final GlobalKey<FormState> _formkeyValue = new GlobalKey<FormState>();
+
+
+    if (imageFile == null) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text("Make a photo\nor pick an image",
+                style: Theme.of(context).primaryTextTheme.display3,
+                textAlign: TextAlign.center),
+            NiceButton(
+              radius: 20,
+              padding: const EdgeInsets.all(15),
+              text: "Select Image!",
+              background: AppColor.white,
+              gradientColors: [Colors.lightBlueAccent, Colors.lightGreen],
+              onPressed: () {
+                _showChoiceDialog(context);
+              },
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Image.file(
+              imageFile,
+              width: 300,
+              height: 200,
+            ),
+        Form(
+          key: _formKey,
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(horizontal: 1.0),
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(FontAwesomeIcons.hashtag),
+                  hintText: "Enter hashtags",
+                  labelText: "Hashtags",
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+              SizedBox(height: 40),
+              Row(
+                children: <Widget>[
+                  DropdownButton<String>(
+                      items: _pollutians.map((String dropDownStringItem ){
+                        return DropdownMenuItem<String>(
+                          value: dropDownStringItem,
+                          child: Text(dropDownStringItem),
+                        );
+                      }).toList(),
+                      onChanged: (String newValueSelected){
+                        setState(() {
+                          this._currentItemSelected = newValueSelected;
+                        });
+                      },
+                    value: _currentItemSelected,
+                  ),
+                ],
+              ),
             ],
           ),
         ),
+            NiceButton(
+              radius: 30,
+              width:100 ,
+              padding: const EdgeInsets.all(5),
+              text: "Send",
+              background: AppColor.white,
+              gradientColors: [Colors.lightBlueAccent, Colors.lightGreen],
+              onPressed: () {
+                //TODO implement sending to instagram
+              },
+            ),
+          ],
+        ),
       );
-    });
-  }
-
-  Widget _decideImageView(){
-    int selectedIndex = 0;
-
-    if(imageFile == null){
-      return
-        Text("Make a photo\nor pick an image",
-          style: Theme.of(context).primaryTextTheme.display3,
-          textAlign: TextAlign.center);
-    }else{
-      return
-      Image.file(imageFile, width: 600, height: 800,);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    var firstColor = Colors.lightBlueAccent, secondColor = Colors.lightGreen;
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              _decideImageView(),
-              NiceButton(
-                radius: 30,
-                padding: const EdgeInsets.all(15),
-                text: "Select Image!",
-                background: AppColor.white,
-                gradientColors: [firstColor, secondColor],
-                onPressed: (){
-                 _showChoiceDialog(context);
-                },
-              ),
-            ],
-          ),
-        ),
+        padding: const EdgeInsets.only(bottom: 5, top: 5),
+        child: _decideImageView(),
       ),
     );
   }
--- lib/ui/types_of_poll_swiper.dart
+++ lib/ui/types_of_poll_swiper.dart
@@ -17,15 +17,15 @@ class TypesOfPollutionState extends State<TypesOfPollution> {
         itemBuilder: (BuildContext context, int index) {
           //TODO: add list of pollutions - create list of images/screens
           return Image.network(
-            "http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
+            "https://drive.google.com/drive/folders/1_nkkOQInl5y53dST7KuMW_uh56FvLLvO?usp=sharing",
+            fit: BoxFit.contain,
           );
         },
         itemCount: 4,
         viewportFraction: 0.8,
         itemHeight: 500,
         itemWidth: 400,
-        layout: SwiperLayout.CUSTOM,
+        layout: SwiperLayout.DEFAULT,
         customLayoutOption: CustomLayoutOption(startIndex: -1, stateCount: 3)
             .addRotate([-45.0 / 180, 0.0, 45 / 180]).addTranslate([
               new Offset(-350.0, -35.0),
